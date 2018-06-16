c3f70ff7633b5d03f718e21b5358992172e73cae
Poethalphyu <poethalphyu97@gmail.com>
2018-06-16 22:58:07 +0630

    Added ex42

---
+++ ex42.py
@@ -0,0 +1,32 @@
+class Animal(object):
+    pass
+class Dog(Animal):
+    def _init_(self,name):
+        self.name=name
+        class Cat(Animal):
+            def __init__(selff,name):
+                self.name=name
+                class Person(object):
+                    def __init__(self,name):
+                        self.name=name
+                        self.pet=None
+                        class Employee(Person):
+                            def __init__(self,name,salary):
+                                super(Empolyee,self).__init__(name)
+                                self.salary=salary
+                                class Fish(object):
+                                    pass
+                                class Salmon(Fish):
+                                    pass
+                                class Halibut(Fish):
+                                    pass
+                                rover=Dog("Rover")
+                                satan=Cat("Satan")
+                                mary-Person("Mary")
+                                mary.pet=satan
+                                frank=Employee("Frank",120000)
+                                frank.pet=rover
+                                flipper=Fish()
+                                crouse=Salmon()
+                                harry=Halibut()
+
