19bc9bb97afc35492f6bc51d5c4879af129a9b85
Poethalphyu <poethalphyu97@gmail.com>
2018-06-17 06:42:41 +0630

    Added ex44

---
+++ ex44.py
@@ -0,0 +1,9 @@
+class Parent(object):
+    def implict(self):
+        print("PARENT implict()")
+        class Child(Parent):
+            pass
+        dad=Parent()
+        son=Child()
+        dad.implict()
+        son.implict()
