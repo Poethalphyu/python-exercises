b57067a2a8e033f5b8cad8cde202ab2efb57e477
Poethalphyu <poethalphyu97@gmail.com>
2018-06-17 06:38:53 +0630

    Added ex43

---
+++ ex43.py
@@ -0,0 +1,34 @@
+class Scene(object):
+    def enter(self)
+    pass
+class Enginee(object):
+    def __init__(self,scene_map):
+        pass
+    def play(self):
+        pass
+    class Death(Scene):
+        def enter(self)
+        pass
+    class CentralCorridor(Scene):
+        def enter(self):
+            pass
+        class laserWeaponArmory(Scene):
+            def enter(self):
+                pass
+class TheBridge(Scene):
+    def  enter(self):
+        pass
+    class EscapePod(Scene):
+        def enter(self):
+            pass
+        class Map(object):
+            def __inint__(self,start_scee):
+                pass
+            def next_scene(self,scene_name):
+                pass
+            def opening_scene:(self):
+                pass
+            a_map=Map('central_corridor')
+            a_game=Enginee(a_map)
+            a_game.play()
+
