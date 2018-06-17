a2d31604730971022eff6cae9c3a3a565fb616ef
Poethalphyu <poethalphyu97@gmail.com>
2018-06-17 12:22:34 +0630

    Added ex49d.py

---
+++ ex49c.py
@@ -0,0 +1,7 @@
+def peek(word_list):
+    if word_list:
+        word=word_list[0]
+        return word[0]
+    else:
+        return None
+
---
+++ ex49d.py
@@ -0,0 +1,11 @@
+def match(word_list,excepinf):
+    if word_list:
+        word=word_list.pop(0)
+        if word[0]==excepting:
+            return word
+        else:
+            return None
+        else:
+            return None
+
+
