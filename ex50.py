c1afd0428b3555da8274274d0793e0bc24012eec
Poethalphyu <poethalphyu97@gmail.com>
2018-06-18 05:46:23 +0630

    Added ex50

---
+++ ex50.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app=Flask(_name_)
+
+@app.route('/')
+def hello_world():
+    greeting="World"
+    return f'Hello,{greeting}'
+if _name_ == "_main_":
+    if _name_ == "_main_":
+        app.run()
